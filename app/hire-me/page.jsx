3be8886cd4108efa718e74e8874446a7e0938b36
N1shan99 <93864054+N1shan99@users.noreply.github.com>
2026-05-06 10:37:28 +0530

    commented out btn

--- app/hire-me/page.jsx
+++ app/hire-me/page.jsx
@@ -99,7 +99,7 @@ export default function HireMe() {
       </motion.div>
       */}
 
-      {/* 🔥 CTA */}
+      {/*
       <motion.button
         whileHover={{ scale: 1.08 }}
         whileTap={{ scale: 0.95 }}
@@ -143,6 +143,7 @@ export default function HireMe() {
       >
         Try ₹1 Demo
       </motion.button>
+      */}
 
       {/* 🔥 Bottom */}
       <motion.div
